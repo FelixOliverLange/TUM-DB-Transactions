90121dcc495f789aeff7ab7f006616c298053091
Felix Lange <felix.lange@tum.de>
2019-04-29 22:21:10 +0200

    inital IO and project setup

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Lange, Felix on 2019-04-29.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string history;
+    cout << "please insert history in folowing format: w 1 x r 2 x w 2 y r 3 y w 3 z r 1 z";
+    cin >> history;
+    cout << "value entered: " << history;
+}
